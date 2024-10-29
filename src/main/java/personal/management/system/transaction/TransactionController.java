12de2f1fba8d15e98aafc077b4b406b368a955a3
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-10-28 21:07:39 -0300

    feat: initial configuration - graphical interface for Transactions

---
+++ src/main/java/personal/management/system/transaction/TransactionController.java
@@ -0,0 +1,11 @@
+package personal.management.system.transaction;
+
+public class TransactionController {
+
+    public void handleAddTransaction(){}
+
+    public void handleEditTransaction(){}
+
+    public void handleDeleteTransaction(){}
+
+}
