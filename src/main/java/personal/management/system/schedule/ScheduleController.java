12028f5fd11d3945342315ea2d9c59d50d297271
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-10-28 21:08:07 -0300

    feat: initial configuration - graphical interface for Schedules

---
+++ src/main/java/personal/management/system/schedule/ScheduleController.java
@@ -0,0 +1,13 @@
+package personal.management.system.schedule;
+
+public class ScheduleController {
+
+    public void handleAddSchedule(){}
+
+    public void handleDeleteSchedule(){}
+
+    public void handlePaySchedule(){}
+
+    public void handleEditSchedule(){}
+
+}
