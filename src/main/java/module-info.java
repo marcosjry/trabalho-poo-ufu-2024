6c92876949b56e880ce4681f72fc2ff7269fa0ce
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-10-28 21:01:28 -0300

    feat: project configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module personal.management.system.pms {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens personal.management.system to javafx.fxml;
+    exports personal.management.system;
+    exports personal.management.system.login;
+}
