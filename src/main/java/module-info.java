bd7ab8e5125f49915c584e03e69d38ace639c42e
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-10-30 23:16:59 -0300

    fix: impl. configuration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,8 +9,17 @@ module personal.management.system.pms {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires java.sql;
 
-    opens personal.management.system to javafx.fxml;
+    opens personal.management.system.graphic.controllers.login to javafx.fxml;
+    opens personal.management.system.graphic.controllers.home to javafx.fxml;
+    opens personal.management.system.graphic.controllers.budget to javafx.fxml;
+    opens personal.management.system.graphic.controllers.report to javafx.fxml;
+    opens personal.management.system.graphic.controllers.schedule to javafx.fxml;
+    opens personal.management.system.graphic.controllers.transaction to javafx.fxml;
+
+    opens personal.management.system.models to javafx.base;
+
+    exports personal.management.system.graphic.controllers.login;
     exports personal.management.system;
-    exports personal.management.system.login;
 }
